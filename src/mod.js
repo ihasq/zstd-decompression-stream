6535f98b5176405315afc924cc981a14938f9ed1
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2026-03-11 16:22:12 +0900

    wip

--- src/mod.js
+++ src/mod.js
@@ -2,30 +2,27 @@ import { Decompress } from "fzstd";
 
 const EMPTY = new Uint8Array(0);
 
+const toU8 = (chunk) => (
+  (chunk instanceof Uint8Array)    ? chunk
+  : (chunk instanceof ArrayBuffer) ? new Uint8Array(chunk)
+  :                                  new Uint8Array(chunk.buffer, chunk.byteOffset, chunk.byteLength)
+);
+
 export class ZstdDecompressionStream extends TransformStream {
   constructor() {
     let decompressor;
     super({
       start(controller) {
-        decompressor = new Decompress((chunk, isLast) => {
-          if (chunk) controller.enqueue(chunk);
-          if (isLast) controller.terminate();
+        decompressor = new Decompress((chunk) => {
+          controller.enqueue(chunk);
         });
       },
       transform(chunk) {
-        decompressor.push(
-          chunk instanceof Uint8Array
-            ? chunk
-            : new Uint8Array(
-                chunk instanceof ArrayBuffer
-                  ? chunk
-                  : chunk.buffer, chunk.byteOffset, chunk.byteLength
-              )
-        );
+        decompressor.push(toU8(chunk));
       },
       flush() {
         decompressor.push(EMPTY, true);
-        decompressor = undefined; // GC解放
+        decompressor = undefined;
       },
     });
   }
